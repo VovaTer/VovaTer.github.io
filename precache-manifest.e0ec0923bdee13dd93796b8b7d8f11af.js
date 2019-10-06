343025e3d65e94bb18318d4bd75d6243663989b7
Vova Tereschenko <uladzimir.tereschenko@freshly.com>
2019-10-07 01:12:46 +0300

    Updates

---
+++ precache-manifest.e0ec0923bdee13dd93796b8b7d8f11af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6337d3c5181397601459c22cb1681a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc020d06c51e09cb81b",
+    "url": "/static/css/main.487d527c.chunk.css"
+  },
+  {
+    "revision": "01a07207e28cdd33ab0e",
+    "url": "/static/js/2.dc7b6c6e.chunk.js"
+  },
+  {
+    "revision": "4cc020d06c51e09cb81b",
+    "url": "/static/js/main.e30b55ce.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
